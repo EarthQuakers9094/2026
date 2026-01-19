2afe0fcc2522c4b63d9bdc3b2c9ed178fd2c3f9c
CreggEgg <eggcregg@gmail.com>
2026-01-19 10:51:29 -0500

    setup development environment for me at home

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    frc-nix.url = "github:frc4451/frc-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      frc-nix,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ frc-nix.overlays.default ];
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell rec {
+          buildInputs = with pkgs; [
+            libglvnd
+            libGL
+            xorg.libXrandr
+            xorg.libXinerama
+            xorg.libXcursor
+            xorg.libXi
+            wayland
+            clang
+            wpilib.glass
+            wpilib.sysid
+            # jdk17
+            stdenv.cc.cc.lib
+            advantagescope
+            wpilib.allwpilibSources
+            wpilib.wpilib-utility
+            (vscode-with-extensions.override {
+              vscodeExtensions = with vscode-extensions; [
+                wpilib.vscode-wpilib
+              ];
+            })
+          ];
+
+          shellHook = ''
+
+            export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib";
+            export HALSIM_EXTENSIONS="$PWD/build/jni/release/libhalsim_gui.so";
+            export JAVA_HOME="/home/churst/Downloads/wpilib-2025/WPILib_Linux-2025.3.2/jdk/"
+          '';
+        };
+      }
+    );
+
+  # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  #
+  # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+}
+#    advantagescope

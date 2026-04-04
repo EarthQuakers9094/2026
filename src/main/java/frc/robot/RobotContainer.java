e41b1d1fa2f60153ed750390cdbea94d8d6e4ac6
CreggEgg <eggcregg@gmail.com>
2026-04-04 18:09:04 -0400

    post philly win

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
@@ -301,6 +302,11 @@ public class RobotContainer {
     NamedCommands.registerCommand("debug", Commands.print("Debug message from Pathplanner"));
     NamedCommands.registerCommand(
         "extend_hopper", new InstantCommand(() -> servo.setSetpointPWM(0.0)));
+
+    NamedCommands.registerCommand(
+        "temporary reverse spindexer",
+        new ParallelRaceGroup(
+            new ReverseKickerSpindexer(kicker, spindexer), new WaitCommand(0.25)));
 
     // leftStick.button(6).onTrue(NamedCommands.getCommand("extend_hopper"));
 
